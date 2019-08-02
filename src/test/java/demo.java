07c683aefbd1cad99b26aa4b1aade5d17574f147
unknown <yangzj3@ifeng.com>
2019-08-02 18:51:56 +0800

    Initial commit

---
+++ src/test/java/demo.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class demo {
+    @Test
+    public static void main(String[] args) {
+        String[] names={"j","2"};
+        for (String name:names){
+            System.out.println(name);
+        }
+    }
+}
